45c272f60163ceadb856cfb176807f97c143fc04
Vladimir <v1ztep@gmail.com>
2020-10-22 18:37:22 +0300

    Заполнил сайт книгами

---
+++ render_website.py
@@ -0,0 +1,25 @@
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+import json
+
+with open("static/description.json", "r", encoding="utf8") as my_file:
+    books_json = my_file.read()
+
+books_dict = json.loads(books_json)
+
+env = Environment(
+    loader=FileSystemLoader('.'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+
+template = env.get_template('template.html')
+
+rendered_page = template.render(
+    books_dict=books_dict,
+)
+
+with open('index.html', 'w', encoding="utf8") as file:
+    file.write(rendered_page)
+
+server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+server.serve_forever()
