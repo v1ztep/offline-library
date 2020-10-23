803ef8fb67b8fb807266a30421b7f9024f2e3c1e
Vladimir <v1ztep@gmail.com>
2020-10-23 16:00:29 +0300

    Интегрировал livereload

--- render_website.py
+++ render_website.py
@@ -1,25 +1,33 @@
-from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import json
+from livereload import Server
+
+
+def on_reload(dict):
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+
+    template = env.get_template('templates/template.html')
+    rendered_page = template.render(
+        books_dict=dict,
+    )
+
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+    print("Site rebuilded")
+
 
 with open("static/description.json", "r", encoding="utf8") as my_file:
     books_json = my_file.read()
-
 books_dict = json.loads(books_json)
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
+on_reload(books_dict)
 
-template = env.get_template('templates/template.html')
+server = Server()
 
-rendered_page = template.render(
-    books_dict=books_dict,
-)
+server.watch('templates/template.html', on_reload(books_dict))
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+server.serve(root='.')
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
