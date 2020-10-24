7a95b49adb7f4843ce5a6a67558d707d6fffbb8e
Vladimir <v1ztep@gmail.com>
2020-10-24 05:14:48 +0300

    Update render_website.py

--- render_website.py
+++ render_website.py
@@ -1,9 +1,10 @@
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import json
 from livereload import Server
+from more_itertools import chunked
 
 
-def on_reload(dict):
+def on_reload():
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
@@ -11,7 +12,7 @@ def on_reload(dict):
 
     template = env.get_template('templates/template.html')
     rendered_page = template.render(
-        books_dict=dict,
+        group_books_list=group_books_list,
     )
 
     with open('index.html', 'w', encoding="utf8") as file:
@@ -21,13 +22,14 @@ def on_reload(dict):
 
 with open("static/description.json", "r", encoding="utf8") as my_file:
     books_json = my_file.read()
-books_dict = json.loads(books_json)
+books_list = json.loads(books_json)
+group_books_list = list(chunked(books_list, 2))
 
-on_reload(books_dict)
+on_reload()
 
 server = Server()
 
-server.watch('templates/template.html', on_reload(books_dict))
+server.watch('templates/template.html', on_reload)
 
 server.serve(root='.')
 
